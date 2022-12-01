7cb9ef952bd3a1bd10ee0d1fe17314646681be79
René Borner <mail@reneborner.de>
2022-12-01 09:27:00 +0100

    cleanup

--- day1/main.ts
+++ day1/main.ts
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const input = fs.readFileSync("day1/input", "utf8").split('\n\n');
-let sums = new Array()
 
-input.forEach(elf => {
-    sums.push(elf.split('\n').map(x => +x).reduce((sum, foodItem) => { return sum + foodItem }))
-});
+const sums = input.reduce((sum, elf) => {
+    sum.push(elf.split('\n').reduce((sum, foodItem) => { return sum + +foodItem }, 0));
+    return sum;
+}, []);
 
-sums = sums.sort((a, b) => { return a - b });
+sums.sort((a, b) => { return a - b });
 
 console.log(`Part1 ${sums.at(-1)}`);
 console.log(`Part2 ${sums.at(-1) + sums.at(-2) + sums.at(-3)}`);
